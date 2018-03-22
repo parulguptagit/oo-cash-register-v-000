7caeacc94aaa35d6aac82f69823ff5593b35b2dc
Parul Gupta <parulguptasmail@gmail.com>
2018-03-22 18:38:07 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,15 +1,19 @@
 require 'pry'
 class CashRegister
+  
   attr_accessor :total, :discount, :last_transaction
+  attr_reader :items
   
   def initialize(discount = 0)
     @total = 0
     @discount = discount
+    @items = []
   end
   
   def add_item(title, price, quantity = 1)
     @last_transaction = price*quantity
     @total = @total + price*quantity
+    @items << title
   end
   
   def apply_discount()
@@ -20,4 +24,6 @@ class CashRegister
       "After the discount, the total comes to $#{@total.to_i}."
     end
   end
+  
+  
 end
