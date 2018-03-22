5004b3319cf0585167dd1d87d3023a4ab47240d3
Parul Gupta <parulguptasmail@gmail.com>
2018-03-22 18:24:11 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -7,7 +7,7 @@ class CashRegister
     @discount = discount
   end
   
-  def add_item(title, price, quantity = 0)
+  def add_item(title, price, quantity = 1)
     @last_transaction = price*quantity
     @total = @total + price*quantity
   end
